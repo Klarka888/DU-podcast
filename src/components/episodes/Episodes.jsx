8f63836c4e622238903bedeaf8803b13f2e2e438
Klarka888 <91918804+Klarka888@users.noreply.github.com>
2024-02-12 19:46:47 +0100

    Add files via upload

---
+++ src/components/episodes/Episodes.jsx
@@ -0,0 +1,15 @@
+const Episodes = ({num, title, guest}) => {
+    return (
+        
+        <div className="episode">
+      <div className="episode__num">{num}</div>
+      <div className="episode__body">
+        <div className="episode__title">{title}</div>
+        <div className="episode__guest">host: {guest}</div>
+      </div>
+        </div>
+        
+    )
+}
+
+export default Episodes
